1401835728536e8a3a3e80f222f8176303956cd8
vvthanatos <vvthanatos@gmail.com>
2020-07-28 16:09:30 +0300

    exception updated

--- src/Exceptions/ModuleNotFound.php
+++ src/Exceptions/ModuleNotFound.php
@@ -1,4 +1,14 @@
 <?php
+/*
+ * This file is part of Vocabulary.
+ *
+ * (c) Halimon Alexander <vvthanatos@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
 namespace HalimonAlexander\Vocabulary\Exceptions;
 
 use RuntimeException;
